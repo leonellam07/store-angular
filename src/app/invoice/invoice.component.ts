d652cc4be423e1b70dee6b58ffaeb6fc7c2c996c
Leonel Lam <Leokirap014@gmail.com>
2022-10-16 19:36:38 -0600

    Form Invoice

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.sass']
+  styleUrls: ['./invoice.component.scss']
 })
 export class InvoiceComponent implements OnInit {
 
-  constructor() { }
+  formularioBusqueda!: FormGroup;
+  constructor(private fb: FormBuilder,) {
+
+    this.formularioBusqueda = this.fb.group({
+			searchInput: ['', Validators.required],
+		});
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(myForm: FormGroup){
+
+    if(myForm.invalid) return alert("Ingrese texto a buscar")
+
+    console.log(myForm);
+    console.log(myForm.valid);
   }
 
 }
